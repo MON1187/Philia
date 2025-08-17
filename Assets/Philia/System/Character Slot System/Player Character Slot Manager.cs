344014cc0b6f3a27d50b809d4707b14f616665ab
MON1187 <q01040507615@gmail.com>
2025-08-17 22:32:38 +0900

    추가 | 플레이어 캐릭터 배치 테스트용 에셋 및 씬, 스크립트

--- Assets/Philia/System/Character Slot System/Player Character Slot Manager.cs
+++ Assets/Philia/System/Character Slot System/Player Character Slot Manager.cs
@@ -4,11 +4,16 @@ public class PlayerCharacterSlotManager : MonoBehaviour
 {
     public BattleUnitModel[] battleUnitModels = new BattleUnitModel[4];
 
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
     public void BattleUnitModelSaveSlotData(BattleUnitModel battleUnitModel, int location)
     {
         battleUnitModels[location] = battleUnitModel;
     }
-
+     
     public BattleUnitModel LoadSlotUnitModelData(int loaction)
     {
         return battleUnitModels[loaction];
---
+++ Assets/Philia/System/Character Slot System/Player Select Battle Model Slot.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class PlayerBattleModelSkit : MonoBehaviour
+{
+    public void B_SelectCharacter(int location)
+    {
+
+    }
+}
