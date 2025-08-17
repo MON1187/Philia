66110464e24913f1eed8b2687a56c2ed9f77bb2b
MON1187 <q01040507615@gmail.com>
2025-08-18 02:56:46 +0900

    수정 | 캐릭터 슬롯 UI 메니저

--- Assets/Philia/System/Character Slot System/Player Character Slot Manager.cs
+++ Assets/Philia/System/Character Slot System/Player Character Slot Manager.cs
@@ -1,21 +1,67 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Apple;
 
 public class PlayerCharacterSlotManager : MonoBehaviour
 {
+    public static PlayerCharacterSlotManager Instats;
+
     public BattleUnitModel[] battleUnitModels = new BattleUnitModel[4];
+
+    [SerializeField] private CharacterSelectionSystem characterSelectionSystem;
+
+    private int _location;
+
+    private PlayerBattleModelSlot _curModelSlot;
 
     private void Awake()
     {
-        DontDestroyOnLoad(this);
+        if (Instats == null)
+        {
+            Instats = this;
+            DontDestroyOnLoad(Instats);
+        }
+        else
+        {
+            Destroy(this);
+        }
     }
 
     public void BattleUnitModelSaveSlotData(BattleUnitModel battleUnitModel, int location)
     {
         battleUnitModels[location] = battleUnitModel;
     }
-     
+
     public BattleUnitModel LoadSlotUnitModelData(int loaction)
     {
         return battleUnitModels[loaction];
     }
+
+    #region Functions that are performed while selecting a character
+
+    public void SelectCharacterFinalChoice(BattleUnitModel model)
+    {
+        _curModelSlot.UpdateSlot(model);
+
+        BattleUnitModelSaveSlotData(model, _location);
+
+        //UI OFF
+        characterSelectionSystem.OnCharacterModelSelectActivate(false);
+
+        //reset
+        {
+            _curModelSlot = null;
+
+            _location = -1;
+        }
+    }
+
+    public void SelectModelLoacation(PlayerBattleModelSlot slotData,int slotIndedx)
+    {
+        _curModelSlot = slotData;
+
+        _location = slotIndedx;
+    }
+
+    #endregion
 }
