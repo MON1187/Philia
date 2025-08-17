d7cda99c3d48b0ed5d3018e186a2efc02b04e2f2
MON1187 <q01040507615@gmail.com>
2025-08-18 02:56:28 +0900

    수정 | 캐릭터 선택 슬롯 기능 구현

--- Assets/Philia/System/Character Slot System/Player Select Battle Model Slot.cs
+++ Assets/Philia/System/Character Slot System/Player Select Battle Model Slot.cs
@@ -1,9 +1,46 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class PlayerBattleModelSkit : MonoBehaviour
+public class PlayerBattleModelSlot : MonoBehaviour
 {
+    [SerializeField] private CharacterSelectionSystem characterSelectionSystem;
+
+    [SerializeField] private Image characterSpritePlace;
+
+    private BattleUnitModel owner;
+
+    [SerializeField] private int index = 0;
+
+    private void Start()
+    {
+        Button btn = GetComponent<Button>();
+
+        btn.onClick.RemoveAllListeners();
+
+        btn.onClick.AddListener(() => B_SelectCharacter(index));
+    }
+
     public void B_SelectCharacter(int location)
     {
+        //First, a code that tells you which cell you selected
+        PlayerCharacterSlotManager.Instats.SelectModelLoacation(this ,location);
 
+        //Activates the interface to allow character selection.
+        characterSelectionSystem.OnCharacterModelSelectActivate(true);
+    }
+
+    public void UpdateSlot(BattleUnitModel _owner)
+    {
+        owner = _owner;
+        UpdateSprite();
+    }
+
+    private void UpdateSprite()
+    {
+        if (owner != null)
+        {
+            characterSpritePlace.sprite = owner.GetAtmosphericSprite();
+        }
     }
 }
