f93c545ca01fd16a51ffa1cdc42a69ca5ad2ee66
cleans77 <agkTeamC@sdh.hs.kr>
2025-08-26 09:28:31 +0900

    추가 | 미니게임 노드 (예전 노드 시스템 백업 겸)

---
+++ Assets/Philia/System/Mini Game System/Node Game/Mini Node.cs
@@ -0,0 +1,133 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MiniNode : MonoBehaviour
+{
+    //NodeType _nodeType;
+
+    private Button _myButton;
+
+    public string _name;
+
+    public int _loadBattleSceneId;
+
+    private bool _isAvailability = false;
+
+    [SerializeField] private bool isFrist = false;
+
+    public MiniNode[] _nextNodes;
+
+    private MiniNode[] _currentNodes;
+
+    [SerializeField] private MiniNode _previousNodes;
+
+    private void Start()
+    {
+        _myButton = GetComponent<Button>();
+        {
+            if (isFrist)
+                FirstSettingNode();
+            else
+                _myButton.onClick.AddListener(CheckWhetherMove);
+        }
+    }
+
+    private void FirstSettingNode()
+    {
+        NodeSystem.Instance.SetPlayerMoveMark(this.gameObject.transform);
+
+        _isAvailability = false;
+
+        foreach (MiniNode node in _nextNodes)
+        {
+            node.GetPreviousNode(this);
+            node._isAvailability = true;
+        }
+    }
+
+
+    void NodeActivation()
+    {
+        if (!_isAvailability)
+            return;
+
+        GetDeactivateNodes(_nextNodes);
+
+        //전투 씬으로 넘어가는 코드 작성
+
+    }
+
+    private void GetDeactivateNodes(MiniNode[] _curNodes)
+    {
+        for (int i = 0; i < _curNodes.Length; i++)
+        {
+            _curNodes[i]._currentNodes = new MiniNode[_curNodes.Length];
+
+            _curNodes[i]._currentNodes = _curNodes;
+        }
+    }
+
+    /// <summary>
+    /// After the battle process is over, if you win, run the code below
+    /// Function terminates upon defeat.
+    /// </summary>
+    private void NodeUpdate()
+    {
+        if (_isAvailability)
+        {
+            foreach (MiniNode node in _nextNodes)
+            {
+                node.GetPreviousNode(this);
+                node._isAvailability = true;
+            }
+            print("ad");
+        }
+
+        {
+            this._isAvailability = false;
+
+            foreach (MiniNode node in _currentNodes)
+            {
+                node._isAvailability = false;
+            }
+        }
+    }
+
+
+    #region A function that calls the next node code from the current node
+
+    public void GetPreviousNode(MiniNode previousNode)
+    {
+        _previousNodes = previousNode;
+    }
+
+    public void CheckWhetherMove()
+    {
+        if (!_isAvailability)
+            return;
+
+        NodeSystem.Instance.OnPlayerMoveMark(this.transform);
+    }
+
+    public void OnBackMoveMark(Transform playerMark)
+    {
+        StartCoroutine(BackMoveMark(playerMark));
+    }
+
+    private IEnumerator BackMoveMark(Transform playerMark)
+    {
+        float time = 0;
+
+        if (time < 0)
+        {
+            time += Time.deltaTime;
+
+            playerMark.position = Vector3.Lerp(playerMark.position, _previousNodes.transform.position, time);
+
+            yield return null;
+        }
+    }
+
+    #endregion
+}
