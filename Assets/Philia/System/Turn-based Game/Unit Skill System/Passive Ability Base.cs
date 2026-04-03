57d2a797a50115707ddf168ca63f08d6d0231188
MON1187 <q01040507615@gmail.com>
2026-04-04 00:29:08 +0900

    추가 | 패시브 베이스

---
+++ Assets/Philia/System/Turn-based Game/Unit Skill System/Passive Ability Base.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class PassiveAbilityBase : MonoBehaviour
+{
+    protected BattleUnitData owner;
+
+    public BattleUnitData Owner => owner;
+
+    public virtual void OnBattleStart() { }
+
+    public virtual void OnTurnStart() { }
+
+    public virtual void OnTurnEnd() { }
+
+    public virtual void OnDrawCard() { }
+
+    public virtual void OnWinParrying() { }
+
+    public virtual void OnLoseParrying() { }
+
+    public virtual void OnUseCard() { }
+
+    public virtual int UseSkillSlotAdder()
+    {
+        return 0;
+    }
+
+    public virtual int MaxPlayPointAdder()
+    {
+        return 0;
+    }
+}
