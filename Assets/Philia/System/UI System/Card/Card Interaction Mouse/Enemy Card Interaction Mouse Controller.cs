cdc4906c8971352eb5d449491dad69d14da0a647
MON1187 <q01040507615@gmail.com>
2026-04-17 02:18:47 +0900

    추가 | 마우스로 인한 카드 상호작용 스크립트 베이스

---
+++ Assets/Philia/System/UI System/Card/Card Interaction Mouse/Enemy Card Interaction Mouse Controller.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class EnemyCardInteractionMouseController : MonoBehaviour
+{
+    //만들어야 하는거.
+
+    /// <summary>
+    /// 1. 마우스 포인트 반응.
+    /// 2. 카드 정보 표시.
+    /// </summary>
+}
---
+++ Assets/Philia/System/UI System/Card/Card Interaction Mouse/Player Card Interaction Mouse Controller.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class PlayerCardInteractionMouseController : MonoBehaviour
+{
+    //만들어야 하는거.
+
+    /// <summary>
+    /// 1. 마우스 포인트 반응.
+    /// 2. 카드 정보 표시.
+    /// 3. 카드 선택 후, 적 선정.
+    /// 4. 카드 드래그 자동 사용
+    /// 5. ?
+    /// </summary>
+}
